430c289458ee848d7af7f5b91d44d99e79781284
vsukiasyan <vsukiasyan1@hotmail.com>
2022-01-09 08:30:39 -0800

    Initial Commit

---
+++ GiphyClient/GiphyClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  GiphyClientApp.swift
+//  GiphyClient
+//
+//  Created by Vic Sukiasyan on 1/9/22.
+//
+
+import SwiftUI
+
+@main
+struct GiphyClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
